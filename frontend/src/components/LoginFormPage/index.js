4b38a104fac4b49f6c652e7980140a2364138927
Ruidan-Zhang <ruidanzhang973@gmail.com>
2023-04-30 12:22:02 -0700

    Updated footer on log in page

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -88,6 +88,15 @@ function LoginFormPage() {
           <div className='log-in-page-down-statement'>LinkedWith.</div>
           <button className='log-in-page-down-button' onClick={goToSignUp}>Get started</button>
         </div>
+        <div className='log-in-page-footer'>
+          <div>
+            Ruidan Zhang © 2023 <a href = "https://github.com/Ruidan-Zhang" target="_blank" className='log-in-footer-icon' rel="noopener noreferrer"><i class="fa-brands fa-github"></i> </a>
+            <a href = "https://www.linkedin.com/in/ruidan-meredith-zhang/" target="_blank" className='log-in-footer-icon' rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+          </div>
+          <div>
+            Javascript | Express | React | Redux | SqlAlchemy | PostgresSQL | HTML | CSS | AWS | Hosted on Render
+          </div>
+        </div>
       </div>
     </div>
   );
